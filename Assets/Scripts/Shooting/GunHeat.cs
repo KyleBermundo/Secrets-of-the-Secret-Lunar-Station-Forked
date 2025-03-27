dfe8a0eacb62f53da1027dfb99e8e07d7ef0e603
KyleBermundo <bermundokyle7@gmail.com>
2025-03-26 21:07:32 -0400

    Update GunHeat.cs

--- Assets/Scripts/Shooting/GunHeat.cs
+++ Assets/Scripts/Shooting/GunHeat.cs
@@ -2,38 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GunHeat : MonoBehaviour
-{
-    [SerializeField]
-    Renderer[] heatLevel;
-    [SerializeField]
-    Material coolMaterial;
-    [SerializeField]
-    Material hotMaterial;
-    [SerializeField]
-    ShootingScript shootingScript;
+//public class GunHeat : MonoBehaviour
+//{
+    //[SerializeField]
+    //  Renderer[] heatLevel;
+    // [SerializeField]
+    // Material coolMaterial;
+    // [SerializeField]
+    // Material hotMaterial;
+    // [SerializeField]
+    // ShootingScript shootingScript;
 
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // void Start()
+    //  {
+
+    //  }
 
     // Update is called once per frame
-    void Update()
-    { 
-        //another thing we can edit
-        if (shootingScript.ShotsFired > 19) heatLevel[0].material = hotMaterial;
-        else heatLevel[0].material = coolMaterial;
-        if (shootingScript.ShotsFired > 39) heatLevel[1].material = hotMaterial;
-        else heatLevel[1].material = coolMaterial;
-        if (shootingScript.ShotsFired > 59) heatLevel[2].material = hotMaterial;
-        else heatLevel[2].material = coolMaterial;
-        if (shootingScript.ShotsFired > 79) heatLevel[3].material = hotMaterial;
-        else heatLevel[3].material = coolMaterial;
-        if (shootingScript.ShotsFired > 99) heatLevel[4].material = hotMaterial;
-        else heatLevel[4].material = coolMaterial;
-    }
-}
+    // void Update()
+    //  { 
+    //another thing we can edit
+    //       if (shootingScript.ShotsFired > 19) heatLevel[0].material = hotMaterial;
+    //       else heatLevel[0].material = coolMaterial;
+    //     if (shootingScript.ShotsFired > 39) heatLevel[1].material = hotMaterial;
+    //      else heatLevel[1].material = coolMaterial;
+    //      if (shootingScript.ShotsFired > 59) heatLevel[2].material = hotMaterial;
+    //       else heatLevel[2].material = coolMaterial;
+    //     if (shootingScript.ShotsFired > 79) heatLevel[3].material = hotMaterial;
+    //      else heatLevel[3].material = coolMaterial;
+    //      if (shootingScript.ShotsFired > 99) heatLevel[4].material = hotMaterial;
+    //      else heatLevel[4].material = coolMaterial;
+    //  }
+//}
